15883426a51cb03e12f2ad8a3d5a315985439ab8
chop0 <chop@quenda.net>
2021-08-23 17:22:11 -0700

    ok rustfmt

--- acrobatics/src/main.rs
+++ acrobatics/src/main.rs
@@ -1,34 +1,29 @@
 #![feature(asm)]
 
+use std::io::{Read, Write};
 
+use process_memory::Memory;
 
-use std::io::{Write, Read};
-
-use process_memory::{Memory};
-
-use std::net::{TcpListener};
 use message_passing::verify;
+use std::net::TcpListener;
 
-use sysinfo::{SystemExt, ProcessExt};
+use sysinfo::{ProcessExt, SystemExt};
 
-use nix::sys::mman::{mmap, ProtFlags, MapFlags};
-use std::os::raw::c_void;
+use nix::sys::mman::{mmap, MapFlags, ProtFlags};
 use nix::sys::ptrace::traceme;
+use std::os::raw::c_void;
 
 fn main() {
-
-   let lemon = traceme().is_err();
+    let lemon = traceme().is_err();
     let mut writemode = false;
     let mut bytes: Vec<u8> = vec![];
-
 
     if let Ok(listener) = TcpListener::bind("0.0.0.0:3333") {
         let (mut stream, _addr) = listener.accept().unwrap();
 
         //stream.set_read_timeout(Some(Duration::from_millis(100)));
 
-
-       loop {
+        loop {
             let mut buf = [0u8; 16];
             stream.read_exact(&mut buf).unwrap();
             let result = verify(buf, lemon);
@@ -40,14 +35,21 @@ fn main() {
                 continue;
             };
 
-
             //println!("msg received");
             if result == [0x65, 0x91, 0x9, 0x44, 0x00, 0x12, 0x8f, 0xff] {
                 let ptr = unsafe {
-                    mmap(std::ptr::null_mut::<c_void>(), bytes.len(), ProtFlags::PROT_EXEC | ProtFlags::PROT_READ | ProtFlags::PROT_WRITE, MapFlags::MAP_SHARED | MapFlags::MAP_ANONYMOUS, 0, 0).unwrap()
+                    mmap(
+                        std::ptr::null_mut::<c_void>(),
+                        bytes.len(),
+                        ProtFlags::PROT_EXEC | ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                        MapFlags::MAP_SHARED | MapFlags::MAP_ANONYMOUS,
+                        0,
+                        0,
+                    )
+                    .unwrap()
                 } as *mut u8;
-               bytes.push(0xcc);
-              //  println!("mapped {:X?}", ptr);
+                bytes.push(0xcc);
+                //  println!("mapped {:X?}", ptr);
                 unsafe {
                     std::ptr::copy(bytes.as_ptr(), ptr, bytes.len());
                     asm! {
@@ -61,8 +63,6 @@ fn main() {
             } else if result == [0x2d, 0x94, 0x22, 0x9b, 0x15, 0x98, 0x91, 0x11] {
                 writemode = true;
             }
-        };        
-         }
+        }
+    }
 }
-
-
--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,27 +1,23 @@
+use bytes::Bytes;
+use message_passing::sign;
+use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::time::Duration;
-use std::io::{Write, Read};
-use message_passing::sign;
-use bytes::{Bytes};
-
 
 fn main() {
-println!("ddd");
+    println!("ddd");
     let shellcode = Bytes::copy_from_slice(include_bytes!("shellcode"));
     let mut connection = TcpStream::connect("localhost:3333").unwrap();
     connection.set_read_timeout(Some(Duration::from_millis(20)));
     let mut buf = [0u8; 4];
     while &buf != &[1, 1, 1, 1] {
-println!("{:?}", buf);
+        println!("{:?}", buf);
         connection.write(&sign([0x2d, 0x94, 0x22, 0x9b, 0x15, 0x98, 0x91, 0x11]));
         connection.read(&mut buf);
     }
 
-
-
-
     for i in shellcode.chunks(8) {
-println!("sending shellcode chunk");
+        println!("sending shellcode chunk");
         let mut buf = [0u8; 4];
         let mut shellcodebuf = [0x90; 8];
 
@@ -29,13 +25,13 @@ println!("sending shellcode chunk");
             *dst = *src
         }
 
-    //    shellcodebuf.copy_from_slice(i);
+        //    shellcodebuf.copy_from_slice(i);
         while &buf != &[1, 1, 1, 1] {
             connection.write(&sign(shellcodebuf));
             connection.read(&mut buf);
         }
     }
-println!("message sent");
+    println!("message sent");
     let mut buf = [0u8; 4];
     while &buf != &[1, 1, 1, 1] {
         connection.write(&sign([0x65, 0x91, 0x9, 0x44, 0x00, 0x12, 0x8f, 0xff]));
@@ -44,12 +40,15 @@ println!("message sent");
 
     let mut str = [0u8; 10];
 
-println!("waiting for flag");
-loop { // lemonthink
-    connection.read(&mut str);
-let res = std::str::from_utf8(&mut str).unwrap();
-     print!("{}", res);
-if res.contains('}') { break }
-}
-println!();
+    println!("waiting for flag");
+    loop {
+        // lemonthink
+        connection.read(&mut str);
+        let res = std::str::from_utf8(&mut str).unwrap();
+        print!("{}", res);
+        if res.contains('}') {
+            break;
+        }
+    }
+    println!();
 }
